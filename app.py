68b5ff27bcdd22070eecad4f38cc50dbe8f52a96
riteshskr <riteshskr@gmail.com>
2026-01-20 17:04:19 -0800

    admin

--- app.py
+++ app.py
@@ -584,6 +584,21 @@ def admin_login():
     conn, cur = get_db()
 
     if request.method == "POST":
+        import tkinter as tk
+          
+        root = tk.Tk()
+        root.geometry("300x100")
+        root.title("sc1")
+        root.update_idletasks()
+        width = root.winfo_width()
+        height = root.winfo_height()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        x = (screen_width // 2) - (width // 2)
+        y = (screen_height // 2) - (height // 2)
+        root.geometry(f"{width}x{height}+{x}+{y}")
+        name_var = tk.StringVar()
+        passw_var = tk.StringVar()
         u = request.form["username"]
         p = request.form["password"]
 
