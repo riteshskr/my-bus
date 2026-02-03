0c029966b231fda34a20676dba3f0446b64e8e4e
riteshskr <riteshskr@gmail.com>
2026-02-03 01:46:25 -0800

    new

--- app.py
+++ app.py
@@ -947,8 +947,8 @@ def seat_page(sid):
     // ===== Leaflet Map Init =====
     const map = L.map('map').setView([BUS_LAT, BUS_LNG], 10);
 
-    L.tileLayer('https://{{s}}.basemaps.cartocdn.com/light_all/{{z}}/{{x}}/{{y}}{{r}}.png', {{
-        attribution: ''&copy; OpenStreetMap &copy; CARTO',
+    L.tileLayer('https://{{s}}.basemaps.cartocdn.com/dark_all/{{z}}/{{x}}/{{y}}{{r}}.png', {{
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
         subdomains: 'abcd',
         maxZoom: 19
     }}).addTo(map);
