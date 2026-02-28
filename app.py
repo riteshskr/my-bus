719abdd09a98cf90c9ab763c0f1e7b655d53ef00
riteshskr <riteshskr@gmail.com>
2026-02-27 20:46:49 -0800

    28/02/2026

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ Compress(app)
 # ✅ SocketIO Configuration
 socketio = SocketIO(
     app,
-    cors_allowed_origins="*",
-    async_mode="threading"
+    async_mode="threading",
+    cors_allowed_origins="*"
 )
 
 
